c01b2b12fdde87fea8e29f93453588c1b6d7ffbe
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-17 12:56:36 +0530

    Able To post and make relationship between teacher and classes

--- Server/src/index.js
+++ Server/src/index.js
@@ -6,4 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const teacherController = require("./controllers/Teacher.controller");
+const classController = require("./controllers/Classes.controller");
+
+app.use("/teachers", teacherController);
+app.use("/classes", classController);
+
 module.exports = app;
