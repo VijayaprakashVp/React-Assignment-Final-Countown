ce6f8b81b575a4e7f472eaaebfe6a28f41053a0a
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-17 19:32:14 +0530

    Final Update

--- Client/react-final-countdown/src/Components/Home.jsx
+++ Client/react-final-countdown/src/Components/Home.jsx
@@ -3,28 +3,48 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
+  const [sortdata, setSortdata] = useState([]);
 
-  const display = () => {
-    fetch(`http://localhost:8080/teachers?_limit=4&_page=${page}`) //_limit=3&_page=${page}
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res);
-      })
-      .catch((err) => console.log(err));
+  const handleSort = () => {
+    let temp = [...data];
+    temp.sort((a, b) => a.age - b.age);
+    setSortdata(temp);
+  };
+
+  //   console.log("sortdata:", sortdata);
+
+  const handleGender = (value) => {
+    //   let temp = [...data];
+    if (value == "All") {
+      setSortdata(data);
+    } else {
+      let temp = data.filter((e) => e.gender == value);
+      setSortdata(temp);
+    }
   };
 
   useEffect(() => {
-    display();
-  }, [page]);
+    fetch(`http://localhost:8080/teachers`) //?_limit=3&_page=${page}
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setSortdata(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
       <Link to="/login">Login</Link>
-
+      <br />
+      <button onClick={handleSort}>Sort by age</button> <br />
+      <button onClick={() => handleGender("All")}>All</button>
+      <button onClick={() => handleGender("Male")}>Sort by Male</button>
+      <br />
+      <button onClick={() => handleGender("Female")}>Sort by Female</button>
       <h2>Teacher's Details</h2>
       <div className="Cards">
-        {data.map((e) => (
+        {sortdata.map((e) => (
           <div key={e._id}>
             <img
               src={e.img}
@@ -34,17 +54,16 @@ export const Home = () => {
             <h3>Name : {e.name}</h3>
             <p>Age : {e.age}</p>
             <p>Gender : {e.gender}</p>
-            {/* {e.class_id.map((el) => (
-              <div key={el._id}>
+            {e.class_id.map((el) => (
+              <div key={el._id} style={{ border: "1px solid gray" }}>
                 <p>Grade : {el.Grade}</p>
                 <p>Section : {el.Section}</p>
                 <p>Subject : {el.Subject}</p>
               </div>
-            ))} */}
+            ))}
           </div>
         ))}
       </div>
-
       <button onClick={() => setPage(page - 1)}>Prev</button>
       <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
